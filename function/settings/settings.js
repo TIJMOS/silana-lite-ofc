42a40ca0fc221393b75e5436d749b1fad65c5cb0
TIJMOS <mrwanada2025@gmail.com>
2026-02-22 15:19:46 +0000

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,7 +14,7 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
+ nomerbot : '212602688877',
  pairingNumber : '212605784394',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
